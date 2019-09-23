bd7ba4d94f4e053c6f4345e24d98b2f75fbf37aa
omar159525212 <55595771+omar159525212@users.noreply.github.com>
2019-09-23 08:56:21 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,9 +11,9 @@ client.on('message', message => {
     let copy = "King | Bot";
     let request = `Requested By ${message.author.username}`;
     if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
-    msg.react('?')
-    .then(() => msg.react(':white_check_mark: '))
-    .then(() =>msg.react(':negative_squared_cross_mark: '))
+    msg.react('✅')
+    .then(() => msg.react('❌'))
+    .then(() =>msg.react('✅'))
 
     let reaction1Filter = (reaction, user) => reaction.emoji.name === '?' && user.id === message.author.id;
     let reaction2Filter = (reaction, user) => reaction.emoji.name === '?' && user.id === message.author.id;
